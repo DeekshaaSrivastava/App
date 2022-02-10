18ea242c2e5ebb4317e0bc14bf80d95636fcfae1
Sidit Srivastava <49486170+siditsrivastava@users.noreply.github.com>
2022-02-10 16:02:46 +0530

    happy

---
+++ asses/index.js
@@ -0,0 +1,70 @@
+//create an array and passing the number ,question,options,and answers
+
+let questions = [
+  {
+    numb: 1,
+    question: "What does COBOL stand for?",
+    answer: "Common Business Oriented Language",
+    options: [
+      "Contact Buy Operating Language",
+      "Colourful Bonus Other Language",
+      "Contact Brought Optional Language",
+      "Common Business Oriented Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "extensible Markup Language",
+    options: [
+      "extensible Markup Language",
+      "executable Multiple Language",
+      "extra Multi-Program Language",
+      "examine Multiple Language",
+    ],
+  },
+  {
+    numb: 6,
+    question: "What does CPU stand for?",
+    answer: "Central Processing Unit",
+    options: [
+      "Compact Process Unit",
+      "Common Profit Unit",
+      "Central Processing Unit",
+      "Central Process Unit",
+    ],
+  },
+];
